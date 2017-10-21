6be2dc7c3db71b68a5b06207613a79a14564f3e3
Wentao Song <wentaosong@Wentaos-MacBook-Pro.local>
2017-10-20 23:30:27 -0500

    Implemented a table view that displays a list of colors.

---
+++ Color/Color/ColorViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ColorViewController.swift
+//  Color
+//
+//  Created by Wentao Song on 2017/10/20.
+//  Copyright © 2017年 Wentao Song. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var color = ["Red", "Organe", "Yellow", "Green", "Blue", "Purple", "Brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return color.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = color[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
